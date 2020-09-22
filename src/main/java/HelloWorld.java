7bac1a82af39569d590f80229945db7228c2630f
Deep Muni <deep.muni94@gmail.com>
2020-09-22 20:39:02 -0300

    JSON configuration file added

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,7 +1,23 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("Hello World!");
+        Object jsonData = new JSONParser().parse(new FileReader("src/main/resources/configuration.json"));
+
+        JSONObject configuration = (JSONObject) jsonData;
+        String environment = (String) configuration.get("ENV");
+        JSONObject dbDetails = (JSONObject) configuration.get(environment);
+
+        System.out.println("Our environment is : " + environment);
+        System.out.println("Environment details are : " + dbDetails);
+
     }
 
 }
