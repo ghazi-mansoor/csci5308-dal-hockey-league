b5df412f32a7483562ab918f3da7589c27a21c24
Mansoor Ghazi <mansoor.ghazi91@gmail.com>
2020-10-04 01:27:09 -0300

    Added league instantiation interface

--- src/main/java/com/groupten/leagueobjectmodel/IinstantiateLeague.java
+++ src/main/java/com/groupten/leagueobjectmodel/IinstantiateLeague.java
@@ -2,7 +2,14 @@ package com.groupten.leagueobjectmodel;
 
 public interface IinstantiateLeague {
     void addLeague(int leagueID, String leagueName);
-    void addConferenceToLeague(int conferenceID, String conferenceName);
-    void addDivisionToConference(int divisionID, String divisionName);
-    void addTeam(String leagueName, String conferenceName, String divisionName, String teamName);
+
+    void addConference(int conferenceID, String conferenceName);
+    void addConference(int leagueID, String leagueName, int conferenceID, String conferenceName);
+
+    void addDivision(int divisionID, String divisionName);
+    void addDivision(int leagueID, String leagueName, int conferenceID, String conferenceName, int divisionID, String divisionName);
+
+    void addTeam(int teamID, String teamName, String generalManager, String headCoach);
+    void addTeam(String leagueName, String conferenceName, String divisionName, String teamName, String generalManager, String headCoach);
+    void addTeam(int leagueID, String leagueName, int conferenceID, String conferenceName, int divisionID, String divisionName, int teamID, String teamName, String generalManager, String headCoach);
 }
--- src/main/java/com/groupten/leagueobjectmodel/LeagueModel.java
+++ src/main/java/com/groupten/leagueobjectmodel/LeagueModel.java
@@ -1,23 +1,21 @@
 package com.groupten.leagueobjectmodel;
 
-public class LeagueModel implements IinstantiateLeague {
+public class LeagueModel {
     public League league;
     public Conference conference;
     public Division division;
+    public Team team;
 
-    @Override
     public void addLeague(int leagueID, String leagueName) {
         this.league = new League(leagueID, leagueName);
     }
 
-    @Override
     public void addConferenceToLeague(int conferenceID, String conferenceName) {
         int leagueID = league.getLeagueID();
         this.conference = new Conference(leagueID, conferenceID, conferenceName);
         league.addConference(conference);
     }
 
-    @Override
     public void addDivisionToConference(int divisionID, String divisionName) {
         int leagueID = league.getLeagueID();
         int conferenceID = conference.getConferenceID();
@@ -25,7 +23,6 @@ public class LeagueModel implements IinstantiateLeague {
         conference.addDivision(division);
     }
 
-    @Override
     public void addTeam(String leagueName, String conferenceName, String divisionName, String teamName) {
         // TODO 1. Create new team > 2. Add team to the current league that is in-memory via the addTeamToDivision method
     }
