8ce2da8a555e53afc1c83abb34b91d8e07d57ba4
Deep Muni <deep.muni94@gmail.com>
2020-10-06 13:28:14 -0300

    Reading the configuration file

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,22 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class HelloWorld {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException, ParseException {
         System.out.println("Hello World");
+
+        String path = "configuration.json";
+
+        JSONParser parser = new JSONParser();
+        JSONObject jsonData = (JSONObject) parser.parse(new FileReader(path));
+
+        System.out.println(jsonData);
+        System.out.println((String) jsonData.get("DBNAME"));
     }
 }
