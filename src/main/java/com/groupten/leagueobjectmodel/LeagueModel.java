073f0071649b617bbff7811eef8061a9822021ca
Mansoor Ghazi <mansoor.ghazi91@gmail.com>
2020-10-07 23:38:04 -0300

    Refactored to use HashMap instead of ArrayList

--- src/main/java/com/groupten/leagueobjectmodel/LeagueModel.java
+++ src/main/java/com/groupten/leagueobjectmodel/LeagueModel.java
@@ -1,25 +1,27 @@
 package com.groupten.leagueobjectmodel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class LeagueModel {
-    private List<League> leagues;
+    private Map<String, League> leagues;
 
     public LeagueModel() {
-        leagues = new ArrayList<League>();
+        leagues = new HashMap<String, League>();
     }
 
     public boolean addLeagueToModel(League league) {
         int numberOfLeagues = leagues.size();
-        leagues.add(league);
+        leagues.put(league.getLeagueName(), league);
         int numberOfLeaguesPostAdditions = leagues.size();
 
         return numberOfLeaguesPostAdditions == numberOfLeagues + 1;
     }
 
     public void saveLeagueModelToDB() {
-        for (League league : leagues) {
+        for (League league : leagues.values()) {
             league.saveLeagueToDB();
         }
     }
