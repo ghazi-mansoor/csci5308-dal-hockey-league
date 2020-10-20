311e1e36e21bd539836c4bc095c7633f6b5d9cb2
Vishal Sancheti <vishky.sancheti@gmail.com>
2020-10-20 17:13:34 +0530

    [update] DivisionDAO uses new classes

--- src/main/java/com/groupten/dao/database/DivisionDAO.java
+++ src/main/java/com/groupten/dao/database/DivisionDAO.java
@@ -1,32 +1,32 @@
 package com.groupten.dao.database;
 
-import com.groupten.dao.DatabaseConnection;
 import com.groupten.dao.IDivisionDAO;
-import com.groupten.dao.ResultSetOperation;
+import com.groupten.database.StoredProcedure;
 
-import java.sql.CallableStatement;
-import java.sql.Connection;
-import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class DivisionDAO implements IDivisionDAO {
-    DatabaseConnection dbConnectionObj = DatabaseConnection.getDatabaseConnectionObject();
 
     @Override
     public int createDivision(int conferenceId, String divisionName) {
         int divisionId = 0;
 
-        try(Connection con = dbConnectionObj.connect()) {
-            CallableStatement cs = con.prepareCall("{CALL createDivision(?,?,?)}");
-            cs.setInt(1, conferenceId);
-            cs.setString(2, divisionName);
-            cs.registerOutParameter(3, java.sql.Types.INTEGER);
-            cs.executeUpdate();
-            divisionId = cs.getInt(3);
+        StoredProcedure storedProcedure = null;
+        try{
+            storedProcedure = new StoredProcedure("createDivision(?,?,?)");
+            storedProcedure.setParameter(1,conferenceId);
+            storedProcedure.setParameter(2,divisionName);
+            storedProcedure.registerOutputParameterInt(3);
+            storedProcedure.execute();
+            divisionId = storedProcedure.getOutputParameterInt(3);
         } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            if(storedProcedure != null){
+                storedProcedure.cleanup();
+            }
         }
 
         return divisionId;
@@ -36,14 +36,18 @@ public class DivisionDAO implements IDivisionDAO {
     public List<HashMap<String, Object>> getDivisions(String colName, String colValue) {
         List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
 
-        try(Connection con = dbConnectionObj.connect()) {
-            CallableStatement cs = con.prepareCall("{CALL getDivisions(?,?)}");
-            cs.setString(1, colName);
-            cs.setString(2, colValue);
-            ResultSet rs = cs.executeQuery();
-            list = ResultSetOperation.convertResultSetToList(rs);
+        StoredProcedure storedProcedure = null;
+        try{
+            storedProcedure = new StoredProcedure("getDivisions(?,?)");
+            storedProcedure.setParameter(1, colName);
+            storedProcedure.setParameter(2, colValue);
+            list = storedProcedure.executeWithResults();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            if(storedProcedure != null){
+                storedProcedure.cleanup();
+            }
         }
 
         return list;
@@ -51,24 +55,34 @@ public class DivisionDAO implements IDivisionDAO {
 
     @Override
     public void updateDivision(int divisionId, String divisionName) {
-        try(Connection con = dbConnectionObj.connect()) {
-            CallableStatement cs = con.prepareCall("{CALL updateDivision(?,?)}");
-            cs.setInt(1, divisionId);
-            cs.setString(2, divisionName);
-            cs.executeUpdate();
+        StoredProcedure storedProcedure = null;
+        try{
+            storedProcedure = new StoredProcedure("updateDivision(?,?)");
+            storedProcedure.setParameter(1,divisionId);
+            storedProcedure.setParameter(2,divisionName);
+            storedProcedure.execute();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            if(storedProcedure != null){
+                storedProcedure.cleanup();
+            }
         }
     }
 
     @Override
     public void deleteDivision(int divisionId) {
-        try(Connection con = dbConnectionObj.connect()) {
-            CallableStatement cs = con.prepareCall("{CALL deleteDivision(?)}");
-            cs.setInt(1, divisionId);
-            cs.executeUpdate();
+        StoredProcedure storedProcedure = null;
+        try{
+            storedProcedure = new StoredProcedure("deleteDivision(?)");
+            storedProcedure.setParameter(1,divisionId);
+            storedProcedure.execute();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            if(storedProcedure != null){
+                storedProcedure.cleanup();
+            }
         }
     }
 
@@ -76,13 +90,17 @@ public class DivisionDAO implements IDivisionDAO {
     public List<HashMap<String, Object>> getDivisionTeams(int divisionId) {
         List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
 
-        try(Connection con = dbConnectionObj.connect()) {
-            CallableStatement cs = con.prepareCall("{CALL getDivisionTeams(?)}");
-            cs.setInt(1, divisionId);
-            ResultSet rs = cs.executeQuery();
-            list = ResultSetOperation.convertResultSetToList(rs);
+        StoredProcedure storedProcedure = null;
+        try{
+            storedProcedure = new StoredProcedure("getDivisionTeams(?)");
+            storedProcedure.setParameter(1, divisionId);
+            list = storedProcedure.executeWithResults();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally{
+            if(storedProcedure != null){
+                storedProcedure.cleanup();
+            }
         }
 
         return list;
