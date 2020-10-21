ff03cf64396aea0780ddb25f83ee54fb11ad556f
Mansoor Ghazi <mansoor.ghazi91@gmail.com>
2020-10-21 17:32:45 -0300

    Removed older LoM Implementations

--- src/main/java/com/groupten/leagueobjectmodel/leaguemodel/ILeagueModel.java
+++
@@ -1,9 +0,0 @@
-package com.groupten.leagueobjectmodel.leaguemodel;
-
-import com.groupten.leagueobjectmodel.league.League;
-
-public interface ILeagueModel {
-    boolean addLeague(League league);
-    boolean containsLeague(String leagueName);
-    League getLeague(String leagueName);
-}
