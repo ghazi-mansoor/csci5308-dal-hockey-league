ddc20a08b0e506810c232b4ef424e0b379ec7ee7
Vishal Sancheti <vishky.sancheti@gmail.com>
2020-10-26 01:14:50 +0530

    [cleanup] remove fake states from simulation

--- src/main/java/com/groupten/statemachine/simulation/Simulation.java
+++ src/main/java/com/groupten/statemachine/simulation/Simulation.java
@@ -8,44 +8,8 @@ public class Simulation implements ISimulation {
     private IConsole console;
 
     @Override
-    public void beginSimulation() {
+    public void init() {
         console = Injector.injector().getConsoleObject();
         console.printLine("Beginning Simulation");
-    }
-
-    @Override
-    public void fakeState_1() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Fake State 1");
-    }
-
-    @Override
-    public void fakeState_2() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Fake State 2");
-    }
-
-    @Override
-    public void simulate() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Simulation in Progress");
-    }
-
-    @Override
-    public void fakeState_3() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Fake State 3");
-    }
-
-    @Override
-    public void fakeState_4() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Fake State 4");
-    }
-
-    @Override
-    public void endSimulation() {
-        console = Injector.injector().getConsoleObject();
-        console.printLine("Ending Simulation");
     }
 }
