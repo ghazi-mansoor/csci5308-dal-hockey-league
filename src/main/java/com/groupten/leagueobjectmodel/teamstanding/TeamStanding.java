ba881d13ff7fca58c144fe5ae8d19c7531ccdb2d
Vishal Sancheti <vishky.sancheti@gmail.com>
2020-10-27 17:12:12 +0530

    [update] included team name in teamstanding

--- src/main/java/com/groupten/leagueobjectmodel/teamstanding/TeamStanding.java
+++ src/main/java/com/groupten/leagueobjectmodel/teamstanding/TeamStanding.java
@@ -2,23 +2,33 @@ package com.groupten.leagueobjectmodel.teamstanding;
 
 
 public class TeamStanding implements Comparable<TeamStanding> {
+    private String teamName;
     private int points;
     private int leagueRank;
     private int conferenceRank;
     private int divisionRank;
 
     public TeamStanding() {
+        this.teamName = "";
         this.points = 0;
         this.leagueRank = 0;
         this.conferenceRank = 0;
         this.divisionRank = 0;
     }
 
-    public TeamStanding(int points, int leagueRank, int conferenceRank,int divisionRank){
+    public TeamStanding(String teamName, int points, int leagueRank, int conferenceRank,int divisionRank){
         this.points = points;
         this.leagueRank = leagueRank;
         this.conferenceRank = conferenceRank;
         this.divisionRank = divisionRank;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
     }
 
     public int getPoints() {
