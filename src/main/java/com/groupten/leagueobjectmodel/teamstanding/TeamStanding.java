d2053014bc075673833107e91ada2d4b4ed394d8
Vishal Sancheti <vishky.sancheti@gmail.com>
2020-10-28 17:42:30 +0530

    [added] win and loss in team standing

--- src/main/java/com/groupten/leagueobjectmodel/teamstanding/TeamStanding.java
+++ src/main/java/com/groupten/leagueobjectmodel/teamstanding/TeamStanding.java
@@ -7,6 +7,8 @@ public class TeamStanding implements Comparable<TeamStanding> {
     private int leagueRank;
     private int conferenceRank;
     private int divisionRank;
+    private int wins;
+    private int losses;
 
     public TeamStanding() {
         this.teamName = "";
@@ -14,14 +16,18 @@ public class TeamStanding implements Comparable<TeamStanding> {
         this.leagueRank = 0;
         this.conferenceRank = 0;
         this.divisionRank = 0;
+        this.wins = 0;
+        this.losses = 0;
     }
 
-    public TeamStanding(String teamName, int points, int leagueRank, int conferenceRank,int divisionRank){
+    public TeamStanding(String teamName, int points, int leagueRank, int conferenceRank,int divisionRank, int wins, int losses){
         this.teamName = teamName;
         this.points = points;
         this.leagueRank = leagueRank;
         this.conferenceRank = conferenceRank;
         this.divisionRank = divisionRank;
+        this.wins = wins;
+        this.losses = losses;
     }
 
     public String getTeamName() {
@@ -62,6 +68,31 @@ public class TeamStanding implements Comparable<TeamStanding> {
 
     public void setDivisionRank(int divisionRank) {
         this.divisionRank = divisionRank;
+    }
+
+    public int getWins() {
+        return wins;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public int getLosses() {
+        return losses;
+    }
+
+    public void setLosses(int losses) {
+        this.losses = losses;
+    }
+
+    public void addWin(){
+        this.wins = this.wins + 1;
+        this.points = this.points + 2;
+    }
+
+    public void addLoss(){
+        this.losses = this.losses + 1;
     }
 
     @Override
--- src/test/java/com/groupten/leagueobjectmodel/teamstanding/TeamStandingTest.java
+++ src/test/java/com/groupten/leagueobjectmodel/teamstanding/TeamStandingTest.java
@@ -7,73 +7,89 @@ import static org.junit.Assert.*;
 public class TeamStandingTest{
     @Test
     public void getTeamNameTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         assertEquals("Team1",teamStanding.getTeamName());
     }
 
     @Test
     public void setTeamNameTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         teamStanding.setTeamName("Team11");
         assertEquals("Team11",teamStanding.getTeamName());
     }
 
     @Test
     public void getPointsTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         assertEquals(1,teamStanding.getPoints());
     }
 
     @Test
     public void setPointsTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         teamStanding.setPoints(11);
         assertEquals(11,teamStanding.getPoints());
     }
 
     @Test
     public void getLeagueRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         assertEquals(2,teamStanding.getLeagueRank());
     }
 
     @Test
     public void setLeagueRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         teamStanding.setLeagueRank(22);
         assertEquals(22,teamStanding.getLeagueRank());
     }
 
     @Test
     public void getConferenceRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         assertEquals(3,teamStanding.getConferenceRank());
     }
 
     @Test
     public void setConferenceRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         teamStanding.setConferenceRank(33);
         assertEquals(33,teamStanding.getConferenceRank());
     }
 
     @Test
     public void getDivisionRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         assertEquals(4,teamStanding.getDivisionRank());
     }
 
     @Test
     public void setDivisionRankTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
         teamStanding.setDivisionRank(44);
         assertEquals(44,teamStanding.getDivisionRank());
     }
 
     @Test
+    public void addWinTest(){
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
+        teamStanding.addWin();
+        assertEquals(3,teamStanding.getPoints());
+        assertEquals(1,teamStanding.getWins());
+    }
+
+    @Test
+    public void addLossTest(){
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
+        teamStanding.addLoss();
+        assertEquals(1,teamStanding.getPoints());
+        assertEquals(1,teamStanding.getLosses());
+    }
+
+    @Test
     public void compareToTest() {
-        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4);
-        TeamStanding teamStanding2 = new TeamStanding("Team1",5,6,7,8);
+        TeamStanding teamStanding = new TeamStanding("Team1",1,2,3,4,0,0);
+        TeamStanding teamStanding2 = new TeamStanding("Team1",5,6,7,8,0,0);
         assertEquals(-4,teamStanding.compareTo(teamStanding2));
     }
 }
