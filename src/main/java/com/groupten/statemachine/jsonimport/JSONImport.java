75a4b2e77861a8a740b0737264d2a27f4a4eed04
Deep Muni <deep.muni94@gmail.com>
2020-10-30 22:11:29 -0300

    Modified import JSON logic

--- src/main/java/com/groupten/statemachine/jsonimport/JSONImport.java
+++ src/main/java/com/groupten/statemachine/jsonimport/JSONImport.java
@@ -53,9 +53,6 @@ public class JSONImport implements IJSONImport {
     @Override
     public boolean instantiateJSONData() {
 
-        boolean leagueAdded = false, conferenceAdded = false, divisionAdded = false, teamAdded = false,
-                managerAdded = false, coachAdded = false, playerAdded = false;
-
         ILeagueModel leagueModel = Injector.instance().getLeagueModelObject();
         League leagueLOM;
         Conference conferenceLOM;
@@ -68,6 +65,8 @@ public class JSONImport implements IJSONImport {
         JsonObject conference, division, team, headCoach, coach, teamPlayer, freeAgent;
         JsonObject gamePlayConfig, aging, gameResolver, injuries, training, trading;
         JsonArray conferences, divisions, teams, players;
+
+        boolean success = false;
 
         gamePlayConfig = (JsonObject) jsonData.get("gameplayConfig");
         aging = (JsonObject) gamePlayConfig.get("aging");
@@ -98,147 +97,151 @@ public class JSONImport implements IJSONImport {
         leagueLOM = new League(leagueName, averageRetirementAge, maximumAge, randomWinChance, randomInjuryChance, injuryDaysLow,
                 injuryDaysHigh, daysUntilStatIncreaseCheck, lossPoint, randomTradeOfferChance, maxPlayersPerTrade, randomAcceptanceChance);
 
-        for (int i = 0; i < conferences.size(); i++) {
-            conference = (JsonObject) conferences.get(i);
-            divisions = (JsonArray) conference.get("divisions");
-            String conferenceName = conference.get("conferenceName").getAsString();
+        try {
+            for (int i = 0; i < conferences.size(); i++) {
+                conference = (JsonObject) conferences.get(i);
+                divisions = (JsonArray) conference.get("divisions");
+                String conferenceName = conference.get("conferenceName").getAsString();
 
-            conferenceLOM = new Conference(conferenceName);
+                conferenceLOM = new Conference(conferenceName);
 
-            for (int j = 0; j < divisions.size(); j++) {
-                division = (JsonObject) divisions.get(j);
-                teams = (JsonArray) division.get("teams");
-                String divisionName = division.get("divisionName").getAsString();
+                for (int j = 0; j < divisions.size(); j++) {
+                    division = (JsonObject) divisions.get(j);
+                    teams = (JsonArray) division.get("teams");
+                    String divisionName = division.get("divisionName").getAsString();
 
-                divisionLOM = new Division(divisionName);
+                    divisionLOM = new Division(divisionName);
 
-                for (int k = 0; k < teams.size(); k++) {
-                    team = (JsonObject) teams.get(k);
-                    String teamName = team.get("teamName").getAsString();
-                    String generalManager = team.get("generalManager").getAsString();
-                    headCoach = (JsonObject) team.get("headCoach");
-                    String coachName = headCoach.get("name").getAsString();
-                    double coachSkating = headCoach.get("skating").getAsDouble();
-                    double coachShooting = headCoach.get("shooting").getAsDouble();
-                    double coachChecking = headCoach.get("checking").getAsDouble();
-                    double coachSaving = headCoach.get("saving").getAsDouble();
-                    players = (JsonArray) team.get("players");
+                    for (int k = 0; k < teams.size(); k++) {
+                        team = (JsonObject) teams.get(k);
+                        String teamName = team.get("teamName").getAsString();
+                        String generalManager = team.get("generalManager").getAsString();
+                        headCoach = (JsonObject) team.get("headCoach");
+                        String coachName = headCoach.get("name").getAsString();
+                        double coachSkating = headCoach.get("skating").getAsDouble();
+                        double coachShooting = headCoach.get("shooting").getAsDouble();
+                        double coachChecking = headCoach.get("checking").getAsDouble();
+                        double coachSaving = headCoach.get("saving").getAsDouble();
+                        players = (JsonArray) team.get("players");
 
-                    teamLOM = new Team(teamName);
-                    managerLOM = new GeneralManager(generalManager);
-                    coachLOM = new Coach(coachName, coachSkating, coachShooting, coachChecking, coachSaving);
+                        teamLOM = new Team(teamName);
+                        managerLOM = new GeneralManager(generalManager);
+                        coachLOM = new Coach(coachName, coachSkating, coachShooting, coachChecking, coachSaving);
 
-                    if (teamLOM.setGeneralManager(managerLOM)) {
-                        managerAdded = true;
-                    } else {
-                        return false;
-                    }
+                        if (teamLOM.setGeneralManager(managerLOM)) {
+                            success = true;
+                        }else{
+                            throw new Exception("Issue with JSON Data");
+                        }
 
-                    if (teamLOM.setHeadCoach(coachLOM)) {
-                        coachAdded = true;
-                    } else {
-                        return false;
-                    }
+                        if (teamLOM.setHeadCoach(coachLOM)) {
+                            success = true;
+                        }else{
+                            throw new Exception("Issue with JSON Data");
+                        }
 
-                    for (int l = 0; l < players.size(); l++) {
-                        teamPlayer = (JsonObject) players.get(l);
-                        String playerName = teamPlayer.get("playerName").getAsString();
-                        String position = teamPlayer.get("position").getAsString();
-                        boolean captain = teamPlayer.get("captain").getAsBoolean();
-                        double playerAge = teamPlayer.get("age").getAsDouble();
-                        double playerSkating = teamPlayer.get("skating").getAsDouble();
-                        double playerShooting = teamPlayer.get("shooting").getAsDouble();
-                        double playerChecking = teamPlayer.get("checking").getAsDouble();
-                        double playerSaving = teamPlayer.get("saving").getAsDouble();
+                        for (int l = 0; l < players.size(); l++) {
+                            teamPlayer = (JsonObject) players.get(l);
+                            String playerName = teamPlayer.get("playerName").getAsString();
+                            String position = teamPlayer.get("position").getAsString();
+                            boolean captain = teamPlayer.get("captain").getAsBoolean();
+                            double playerAge = teamPlayer.get("age").getAsDouble();
+                            double playerSkating = teamPlayer.get("skating").getAsDouble();
+                            double playerShooting = teamPlayer.get("shooting").getAsDouble();
+                            double playerChecking = teamPlayer.get("checking").getAsDouble();
+                            double playerSaving = teamPlayer.get("saving").getAsDouble();
 
-                        playerLOM = new Player(playerName, position, captain, playerAge, playerSkating, playerShooting, playerChecking, playerSaving);
+                            playerLOM = new Player(playerName, position, captain, playerAge, playerSkating, playerShooting, playerChecking, playerSaving);
 
-                        if (teamLOM.addPlayer(playerLOM)) {
-                            playerAdded = true;
-                        } else {
-                            return false;
+                            if (teamLOM.addPlayer(playerLOM)) {
+                                success = true;
+                            }else{
+                                throw new Exception("Issue with JSON Data");
+                            }
+                        }
+
+                        if (divisionLOM.addTeam(teamLOM)) {
+                            success = true;
+                        }else{
+                            throw new Exception("Issue with JSON Data");
                         }
                     }
-
-                    if (divisionLOM.addTeam(teamLOM)) {
-                        teamAdded = true;
-                    } else {
-                        return false;
+                    if (conferenceLOM.addDivision(divisionLOM)) {
+                        success = true;
+                    }else{
+                        throw new Exception("Issue with JSON Data");
                     }
                 }
-                if (conferenceLOM.addDivision(divisionLOM)) {
-                    divisionAdded = true;
-                } else {
-                    return false;
+                if (leagueLOM.addConference(conferenceLOM)) {
+                    success = true;
+                }else{
+                    throw new Exception("Issue with JSON Data");
+                }
+
+            }
+
+            JsonArray freeAgents = (JsonArray) jsonData.get("freeAgents");
+            for (int i = 0; i < freeAgents.size(); i++) {
+                freeAgent = (JsonObject) freeAgents.get(i);
+                String playerName = freeAgent.get("playerName").getAsString();
+                String position = freeAgent.get("position").getAsString();
+                double playerAge = freeAgent.get("age").getAsDouble();
+                double playerSkating = freeAgent.get("skating").getAsDouble();
+                double playerShooting = freeAgent.get("shooting").getAsDouble();
+                double playerChecking = freeAgent.get("checking").getAsDouble();
+                double playerSaving = freeAgent.get("saving").getAsDouble();
+
+                playerLOM = new Player(playerName, position, playerAge, playerSkating, playerShooting, playerChecking, playerSaving);
+
+                if (leagueLOM.addFreeAgent(playerLOM)) {
+                    success = true;
+                }else{
+                    throw new Exception("Issue with JSON Data");
                 }
             }
-            if (leagueLOM.addConference(conferenceLOM)) {
-                conferenceAdded = true;
-            } else {
-                return false;
+
+            JsonArray coaches = (JsonArray) jsonData.get("coaches");
+
+            for (int i = 0; i < coaches.size(); i++) {
+                coach = (JsonObject) coaches.get(i);
+                String coachName = coach.get("name").getAsString();
+                double coachSkating = coach.get("skating").getAsDouble();
+                double coachShooting = coach.get("shooting").getAsDouble();
+                double coachChecking = coach.get("checking").getAsDouble();
+                double coachSaving = coach.get("saving").getAsDouble();
+
+                coachLOM = new Coach(coachName, coachSkating, coachShooting, coachChecking, coachSaving);
+
+                if (leagueLOM.addCoach(coachLOM)) {
+                    success = true;
+                }else{
+                    throw new Exception("Issue with JSON Data");
+                }
             }
 
-        }
+            JsonArray generalManagers = (JsonArray) jsonData.get("generalManagers");
 
-        JsonArray freeAgents = (JsonArray) jsonData.get("freeAgents");
-        for (int i = 0; i < freeAgents.size(); i++) {
-            freeAgent = (JsonObject) freeAgents.get(i);
-            String playerName = freeAgent.get("playerName").getAsString();
-            String position = freeAgent.get("position").getAsString();
-            double playerAge = freeAgent.get("age").getAsDouble();
-            double playerSkating = freeAgent.get("skating").getAsDouble();
-            double playerShooting = freeAgent.get("shooting").getAsDouble();
-            double playerChecking = freeAgent.get("checking").getAsDouble();
-            double playerSaving = freeAgent.get("saving").getAsDouble();
+            for (int i = 0; i < generalManagers.size(); i++) {
+                String generalManager = generalManagers.get(i).getAsString();
 
-            playerLOM = new Player(playerName, position, playerAge, playerSkating, playerShooting, playerChecking, playerSaving);
+                managerLOM = new GeneralManager(generalManager);
 
-            if (leagueLOM.addFreeAgent(playerLOM)) {
-                playerAdded = true;
-            } else {
-                return false;
+                if (leagueLOM.addGeneralManager(managerLOM)) {
+                    success = true;
+                } else {
+                    throw new Exception("Issue with JSON Data");
+                }
             }
-        }
 
-        JsonArray coaches = (JsonArray) jsonData.get("coaches");
-
-        for (int i = 0; i < coaches.size(); i++) {
-            coach = (JsonObject) coaches.get(i);
-            String coachName = coach.get("name").getAsString();
-            double coachSkating = coach.get("skating").getAsDouble();
-            double coachShooting = coach.get("shooting").getAsDouble();
-            double coachChecking = coach.get("checking").getAsDouble();
-            double coachSaving = coach.get("saving").getAsDouble();
-
-            coachLOM = new Coach(coachName, coachSkating, coachShooting, coachChecking, coachSaving);
-
-            if (leagueLOM.addCoach(coachLOM)) {
-                coachAdded = true;
+            if (leagueModel.setCurrentLeague(leagueLOM)) {
+                success = true;
             } else {
-                return false;
+                throw new Exception("Issue with JSON Data");
             }
+        } catch (Exception e){
+            success = false;
         }
 
-        JsonArray generalManagers = (JsonArray) jsonData.get("generalManagers");
-
-        for (int i = 0; i < generalManagers.size(); i++) {
-            String generalManager = generalManagers.get(i).getAsString();
-
-            managerLOM = new GeneralManager(generalManager);
-
-            if (leagueLOM.addGeneralManager(managerLOM)) {
-                managerAdded = true;
-            } else {
-                return false;
-            }
-        }
-
-        if (leagueModel.setCurrentLeague(leagueLOM)) {
-            leagueAdded = true;
-        } else {
-            return false;
-        }
-
-        return leagueAdded & conferenceAdded && divisionAdded && teamAdded && managerAdded && coachAdded && playerAdded;
+        return success;
     }
 }
