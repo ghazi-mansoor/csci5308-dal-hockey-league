64f1648bd2cd89c32b2c02251dd164f73f1e2e55
Mansoor Ghazi <mansoor.ghazi91@gmail.com>
2020-11-30 02:33:14 -0400

    Added player builder to generatePlayers

--- src/main/java/com/groupten/statemachine/simulation/draft/generateplayers/PlayersGenerator.java
+++ src/main/java/com/groupten/statemachine/simulation/draft/generateplayers/PlayersGenerator.java
@@ -8,10 +8,7 @@ import com.groupten.leagueobjectmodel.player.PlayerBuilder;
 import com.groupten.leagueobjectmodel.player.PlayerPosition;
 import com.groupten.statemachine.simulation.draft.DraftConstants;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class PlayersGenerator implements IPlayersGenerator {
     private Map<String, List<Player>> playersGenerated = new HashMap<>();
@@ -27,31 +24,52 @@ public class PlayersGenerator implements IPlayersGenerator {
 
     @Override
     public Map<String, List<Player>> generatePlayers() {
-        List<Player> forwardPlayers = buildPlayersForPosition(PlayerPosition.FORWARD.name());
+        List<Player> forwardPlayers = buildPlayersForPosition(PlayerPosition.FORWARD.name(), numberOfForwardPlayers);
         playersGenerated.put("forward players", forwardPlayers);
         return playersGenerated;
     }
 
-    private List<Player> buildPlayersForPosition(String position) {
+    private List<Player> buildPlayersForPosition(String position, int numberOfPlayers) {
         List<Player> players = new ArrayList<>();
         IPlayerBuilder playerBuilder = Injector.instance().getPlayerBuilder();
+        double minimumAge = 0.0;
+        double maximumAge = 0.0;
+        double minimumSkatingStat = 0.0;
+        double maximumSkatingStat = 0.0;
+        double minimumShootingStat = 0.0;
+        double maximumShootingStat = 0.0;
+        double minimumCheckingStat = 0.0;
+        double maximumCheckingStat = 0.0;
+        double minimumSavingStat = 0.0;
+        double maximumSavingStat = 0.0;
 
         if (position.equals(PlayerPosition.FORWARD.name())) {
-            double minimumAge = ForwardPlayerStatsRanges.MINIMUM_AGE.getDoubleValue();
-            double maximumAge = ForwardPlayerStatsRanges.MAXIMUM_AGE.getDoubleValue();
-            double minimumSkatingStat = ForwardPlayerStatsRanges.MINIMUM_SKATING_STAT.getDoubleValue();
-            double maximumSkatingStat = ForwardPlayerStatsRanges.MAXIMUM_SKATING_STAT.getDoubleValue();
-            double minimumShootingStat = ForwardPlayerStatsRanges.MINIMUM_SHOOTING_STAT.getDoubleValue();
-            double maximumShootingStat = ForwardPlayerStatsRanges.MAXIMUM_SHOOTING_STAT.getDoubleValue();
-            double minimumCheckingStat = ForwardPlayerStatsRanges.MINIMUM_CHECKING_STAT.getDoubleValue();
-            double maximumCheckingStat = ForwardPlayerStatsRanges.MAXIMUM_CHECKING_STAT.getDoubleValue();
-            double minimumSavingStat = ForwardPlayerStatsRanges.MINIMUM_SAVING_STAT.getDoubleValue();
-            double maximumSavingStat = ForwardPlayerStatsRanges.MAXIMUM_SAVING_STAT.getDoubleValue();
+            minimumAge = ForwardPlayerStatsRanges.MINIMUM_AGE.getDoubleValue();
+            maximumAge = ForwardPlayerStatsRanges.MAXIMUM_AGE.getDoubleValue();
+            minimumSkatingStat = ForwardPlayerStatsRanges.MINIMUM_SKATING_STAT.getDoubleValue();
+            maximumSkatingStat = ForwardPlayerStatsRanges.MAXIMUM_SKATING_STAT.getDoubleValue();
+            minimumShootingStat = ForwardPlayerStatsRanges.MINIMUM_SHOOTING_STAT.getDoubleValue();
+            maximumShootingStat = ForwardPlayerStatsRanges.MAXIMUM_SHOOTING_STAT.getDoubleValue();
+            minimumCheckingStat = ForwardPlayerStatsRanges.MINIMUM_CHECKING_STAT.getDoubleValue();
+            maximumCheckingStat = ForwardPlayerStatsRanges.MAXIMUM_CHECKING_STAT.getDoubleValue();
+            minimumSavingStat = ForwardPlayerStatsRanges.MINIMUM_SAVING_STAT.getDoubleValue();
+            maximumSavingStat = ForwardPlayerStatsRanges.MAXIMUM_SAVING_STAT.getDoubleValue();
         }
 
-        for (int i = 0; i < numberOfForwardPlayers; i++) {
+        for (int i = 0; i < numberOfPlayers; i++) {
             playerBuilder.reset();
-            playerBuilder.setProfile("Certain Player", position);
+            String playerName = "Player " + i;
+            playerBuilder.setProfile(playerName, position);
+
+            double playerAge = generateRandomValueBetweenInterval(minimumAge, maximumAge);
+            playerBuilder.setAge(playerAge);
+
+            double skatingStat = generateRandomValueBetweenInterval(minimumSkatingStat, maximumSkatingStat);
+            double shootingStat = generateRandomValueBetweenInterval(minimumShootingStat, maximumShootingStat);
+            double checkingStat = generateRandomValueBetweenInterval(minimumCheckingStat, maximumCheckingStat);
+            double savingStat = generateRandomValueBetweenInterval(minimumSavingStat, maximumSavingStat);
+            playerBuilder.setPlayerStats(skatingStat, shootingStat, checkingStat, savingStat);
+
         }
 
         return null;
@@ -76,6 +94,9 @@ public class PlayersGenerator implements IPlayersGenerator {
         return (int) Math.round(DraftConstants.NUMBER_OF_ROUNDS.getIntValue() * leagueModel.getTotalNumberOfTeams() * percentageOfTotalPlayers);
     }
 
-
+    private double generateRandomValueBetweenInterval(double intervalStart, double intervalEnd) {
+        double random = new Random().nextDouble();
+        return intervalStart + (random * (intervalEnd - intervalStart));
+    }
 
 }
